8da84bc92c55e82e1ecdea1e8c159ab6eaafd018
Martin Paskov <kartaggen@email.com>
2024-07-23 17:44:11 +0300

    Module 9 - remove old classes 2

--- petstorepetservice/src/main/java/com/chtrembl/petstore/pet/data/PetRepository.java
+++
@@ -1,8 +0,0 @@
-package com.chtrembl.petstore.pet.data;
-
-import com.chtrembl.petstore.pet.model.Pet;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PetRepository extends JpaRepository<Pet, Long> {
-
-}
--- petstoreproductservice/src/main/java/com/chtrembl/petstore/product/data/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.chtrembl.petstore.product.data;
-
-import com.chtrembl.petstore.product.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
