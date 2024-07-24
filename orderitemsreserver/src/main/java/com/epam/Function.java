68678758ec4ea25bf134b546a0e27ad74e328491
Martin Paskov <kartaggen@email.com>
2024-07-24 20:11:32 +0300

    Module 9 - Update to ServiceBus Trigger

--- orderitemsreserver/src/main/java/com/epam/Function.java
+++ orderitemsreserver/src/main/java/com/epam/Function.java
@@ -4,45 +4,44 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonSyntaxException;
-import com.microsoft.azure.functions.*;
-import com.microsoft.azure.functions.annotation.*;
-
-import java.util.Optional;
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.OutputBinding;
+import com.microsoft.azure.functions.annotation.BlobInput;
+import com.microsoft.azure.functions.annotation.BlobOutput;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.ServiceBusQueueTrigger;
 
 public class Function {
 
     @FunctionName("reserve-order")
-    public HttpResponseMessage run(
-            @HttpTrigger(name = "req", methods = {HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
+    public void run(
+            @ServiceBusQueueTrigger(name = "message", queueName = "orderqueue", connection = "your-connection-name") String order,
             @BlobInput(name = "inputBlob", connection = "MyStorageConnectionAppSetting", path = "ps-storage/orders.json") String inputBlob,
             @BlobOutput(name = "outputBlob", connection = "MyStorageConnectionAppSetting", path = "ps-storage/orders.json") OutputBinding<String> outputBlob,
             final ExecutionContext context) {
 
         JsonArray ordersJson = JsonParser.parseString(inputBlob).getAsJsonArray();
 
-        Optional<String> body = request.getBody();
-        if (body.isEmpty() || body.get().isEmpty()) {
-            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Missing body!").build();
+        if (order == null || order.isEmpty()) {
+            throw new IllegalArgumentException("Missing order!");
         }
 
         JsonObject newOrder;
         try {
-            newOrder = JsonParser.parseString(body.get()).getAsJsonObject();
+            newOrder = JsonParser.parseString(order).getAsJsonObject();
         } catch (JsonSyntaxException e) {
-            context.getLogger().severe("Body is not a valid JSON: " + body.get());
-            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Invalid JSON body!").build();
+            context.getLogger().severe("Body is not a valid JSON: " + order);
+            throw new IllegalArgumentException("Invalid JSON order!");
         }
 
         String id = newOrder.get("id").getAsString();
         if (id == null || id.isEmpty()) {
-            context.getLogger().severe("Invalid id: " + id + " in body: " + body.get());
-            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Invalid ID body!").build();
+            context.getLogger().severe("Invalid id: " + id + " in body: " + order);
+            throw new IllegalArgumentException("Invalid ID in order!");
         }
 
         updateOrder(ordersJson, newOrder);
         outputBlob.setValue(ordersJson.toString());
-
-        return request.createResponseBuilder(HttpStatus.OK).build();
     }
 
     public void updateOrder(JsonArray allOrders, JsonObject newOrder) {
